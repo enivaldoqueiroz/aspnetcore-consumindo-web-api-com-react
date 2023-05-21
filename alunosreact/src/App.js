9e0dcdca682264f18f9f90b4656da411cc6c2919
Enivaldo Queiroz <enivaldo.jecq@gmail.com>
2023-05-20 22:11:12 -0300

    #Implementando a funcionalidade para incluir um novo aluno, criação da Modal para inserir os dados do novo aluno.

--- alunosreact/src/App.js
+++ alunosreact/src/App.js
@@ -11,9 +11,24 @@ function App() {
   //Endereço para acessar a API dos alunos
   const baseUrl="https://localhost:44379/api/alunos";
 
+  //Hook useState enviar um resquest para API Usando AXIOS
   const [data, setData]=useState([]);
 
-  //Hook useState enviar um resquest para API Usando AXIOS
+  const [modalIncluir, setModalIncluir]=useState(false);
+
+  const [alunoSelecionado, setAlunoSelecionado]=useState(
+  {
+    id: '',
+    nome: '',
+    email: '',
+    idade: ''
+  });
+
+  const abrirFecharModalInluir=()=>{
+    setModalIncluir(!modalIncluir);
+  }
+
+  //Metodo para trazer uma resquest para API Usando AXIOS
   const pedidoGet = async() =>{
     await axios.get(baseUrl)
     .then(response => {
@@ -22,19 +37,41 @@ function App() {
       console.log(error);
     })
   }
+
+const pedidoPost=async()=>{
+  delete alunoSelecionado.id;
+  alunoSelecionado.idade=parseInt(alunoSelecionado.idade);
+    await axios.post(baseUrl, alunoSelecionado)
+  .then(response=>{
+    setData(data.concat(response.data));
+    abrirFecharModalInluir();
+  }).catch(error=>{
+    console.log(error);
+  })
+}
   
   //Hook useEffect
   useEffect(()=>{
     pedidoGet();
   })
 
+
+  const handleChange=e=>{
+    const {name, value}=e.target;
+    setAlunoSelecionado({
+      ...alunoSelecionado,
+      [name]:value
+    });
+    console.log(alunoSelecionado)
+  }
+
   return (
-    <div className="App">
+    <div className="aluno-container">
       <br/>
       <h3>Cadastro de Alunos</h3>
       <header>
         <img src={logoCadastro} alt='Cadastro'></img>
-        <button className='btn btn-success'>Add Novo Aluno</button>
+        <button className='btn btn-success' onClick={()=>abrirFecharModalInluir()}>Add Novo Aluno</button>
       </header>
       <table className='table table-bordered'>
         <thead>
@@ -61,6 +98,29 @@ function App() {
           ))}
         </tbody>
       </table>
+      <Modal isOpen={modalIncluir}>
+        <ModalHeader>Incluir Aluno</ModalHeader>
+        <ModalBody>
+            <div className='form-group'>
+              <label>Nome: </label>
+              <br/>
+              <input type='text' className='Form-control' name='nome' onChange={handleChange}/>
+              <br/>
+              <label>Email: </label>
+              <br/>
+              <input type='text' className='Form-control'name='email' onChange={handleChange}/>
+              <br/>
+              <label>Idade: </label>
+              <br/>
+              <input type='text' className='Form-control'name='idade' onChange={handleChange}/>
+              <br/>
+            </div>
+        </ModalBody>
+        <ModalFooter>
+          <button className='btn btn-primary' onClick={()=>pedidoPost()}>Incluir</button>{" "}
+          <button className='btn btn-danger' onClick={()=>abrirFecharModalInluir()}>Cancelar</button>
+        </ModalFooter>
+      </Modal>
     </div>
   );
 }
