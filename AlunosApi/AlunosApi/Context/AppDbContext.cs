c7478c56bc1e68815cadac248ea8c2abd20cd7a3
Enivaldo Queiroz <enivaldo.jecq@gmail.com>
2023-05-25 21:53:36 -0300

    # Implementação do serviço de autenticação.

--- AlunosApi/AlunosApi/Context/AppDbContext.cs
+++ AlunosApi/AlunosApi/Context/AppDbContext.cs
@@ -1,4 +1,6 @@
 ﻿using AlunosApi.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -7,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace AlunosApi.Controllers
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<IdentityUser>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
         {
@@ -16,24 +18,24 @@ namespace AlunosApi.Controllers
 
         public DbSet<Aluno> Alunos { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Aluno>().HasData(
-            new Aluno
-            {
-                Id = 1,
-                Nome = "Jose",
-                Email = "email@gmail.com",
-                Idade = 23
-            },
-             new Aluno
-             {
-                 Id = 2,
-                 Nome = "Manuela",
-                 Email = "manuela@gmail.com",
-                 Idade = 22
-             }
-            );
-        }
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Aluno>().HasData(
+        //    new Aluno
+        //    {
+        //        Id = 1,
+        //        Nome = "Jose",
+        //        Email = "email@gmail.com",
+        //        Idade = 23
+        //    },
+        //     new Aluno
+        //     {
+        //         Id = 2,
+        //         Nome = "Manuela",
+        //         Email = "manuela@gmail.com",
+        //         Idade = 22
+        //     }
+        //    );
+        //}
     }
 }
