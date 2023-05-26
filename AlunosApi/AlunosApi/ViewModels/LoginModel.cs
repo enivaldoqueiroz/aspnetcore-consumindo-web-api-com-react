34f412cfccded37c8e736e4e5da4f17d47be32d1
Enivaldo Queiroz <enivaldo.jecq@gmail.com>
2023-05-25 23:56:41 -0300

    #Implementação da classe LoginModel

---
+++ AlunosApi/AlunosApi/ViewModels/LoginModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlunosApi.ViewModels
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Email é Obrigatório")]
+        [EmailAddress(ErrorMessage = "Formato de email inválido")]
+        public string Email { get; set; }
+        
+        [Required(ErrorMessage = "A senha é obrigatória")]
+        [StringLength(20, ErrorMessage = "A {0} deve ter no mínimo {1} e no máximo {2} caracteres", MinimumLength = 10)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
