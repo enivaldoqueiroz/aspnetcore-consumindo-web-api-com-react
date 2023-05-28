58273be2be7900f4e0ec604dcab6ccd4187020a6
Enivaldo Queiroz <enivaldo.jecq@gmail.com>
2023-05-28 11:28:05 -0300

    #Implementação da folha de estilos para o componente Login

--- clientreact/src/pages/Login/index.js
+++ clientreact/src/pages/Login/index.js
@@ -5,15 +5,15 @@ import logoImage from  '../../assets/login-management.png';
 export default function Login(){
     return(
         <div className="login-container">
-            <section className="form"></section>
-            <img src={logoImage} alt="Login" id="login-management"></img>
+            <section className="form">
+            <img src={logoImage} alt="Login" id="img-login-management"></img>
             <form>
-                <h1>Cadastro de Alunos</h1>
+                <h1 id="cadastro-de-alunos">Cadastro de Alunos</h1>
                 <input placeholder="Email"></input>
                 <input type="password" placeholder="Password"></input>
                 <button class="button" type="submit">Login</button>
             </form>
-
+            </section>
         </div>
     );
 }
