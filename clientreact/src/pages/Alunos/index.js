8985c1c87c028b407316a5ab56d5068ca4ec3276
Enivaldo Queiroz <enivaldo.jecq@gmail.com>
2023-05-28 23:28:22 -0300

    #Implemente do compente Alunos, inserção da rota e instalação do pacote react-icons

---
+++ clientreact/src/pages/Alunos/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './styles.css';
+
+import logoCadastro from '../../assets/icon-cadastro.png';
+import { Link } from 'react-router-dom';
+import { FiXCircle } from 'react-icons/fi';
+
+export default function Alunos(){
+    return(
+        <div className='aluno-container'>
+            <header>
+                <img src={logoCadastro} alt='Cadastro'></img>
+                <span>Bem-Vindo, <strong>Desenvolvedor</strong></span>
+                <Link className='button' to="aluno/novo">Novo Aluno</Link>
+                <button type='button'>
+                    <FiXCircle size={35} color='#17202a'></FiXCircle>
+                </button>
+            </header>
+            <form>
+                <input type='text' placeholder='Nome'></input>
+                <button type='button' class='button'>
+                    Filtrar aluno por nome (parcial)
+                </button>
+            </form>
+            <h1>Relação de Alunos</h1>
+        </div>
+    )
+}
--- clientreact/src/routes.js
+++ clientreact/src/routes.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
-import Login from "./pages/Login";
+import Login from './pages/Login';
+import Alunos from './pages/Alunos';
 
 export default function RoutesComponents(){
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" exact Component={Login}/>
+                <Route path="/alunos" Component={Alunos}/>
             </Routes>
         </BrowserRouter>
     );
