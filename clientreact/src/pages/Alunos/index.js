7c018c0beaf91776f00b8c77986dbc5803f2a419
Enivaldo Queiroz <enivaldo.jecq@gmail.com>
2023-06-03 18:35:43 -0300

    #Renderização da lista de alunos e seus nomes, e-mails e idades. Consumindo o endpoint api/alunos

--- clientreact/src/pages/Alunos/index.js
+++ clientreact/src/pages/Alunos/index.js
@@ -1,52 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import './styles.css';
+import api from '../../services/api';
 
 import logoCadastro from '../../assets/icon-cadastro.png';
-import { Link } from 'react-router-dom';
 import { FiXCircle, FiEdit, FiUserX } from 'react-icons/fi';
 
-export default function Alunos(){
-    return(
-        <div className='aluno-container'>
-            <header>
-                <img src={logoCadastro} alt='Cadastro'></img>
-                <span>Bem-Vindo, <strong>Desenvolvedor</strong></span>
-                <Link className='button' to="aluno/novo/0">Novo Aluno</Link>
-                <button type='button'>
-                    <FiXCircle size={35} color='#17202a'></FiXCircle>
-                </button>
-            </header>
-            <form>
-                <input type='text' placeholder='Nome'></input>
-                <button type='button' class='button'>
-                    Filtrar aluno por nome (parcial)
-                </button>
-            </form>
-            <h1>Relação de Alunos</h1>
-            <ul>
-                <li>
-                    <b>Nome: </b>Paulo<br></br>
-                    <b>Email: </b>paulo@email.com<p></p>
-                    <b>Idade: </b>22<b></b>
-                    <button type='button'>
-                        <FiEdit size={25} color='#17202a'></FiEdit>
-                    </button>
-                    <button>
-                        <FiUserX size={25} color='#17202a'></FiUserX>
-                    </button>
-                </li>
-                <li>
-                    <b>Nome: </b>Paulo<br></br>
-                    <b>Email: </b>paulo@email.com<p></p>
-                    <b>Idade: </b>22<b></b>
-                    <button type='button'>
-                        <FiEdit size={25} color='#17202a'></FiEdit>
-                    </button>
-                    <button>
-                        <FiUserX size={25} color='#17202a'></FiUserX>
-                    </button>
-                </li>
-            </ul>
-        </div>
-    )
+export default function Alunos() {
+
+  const [nome, setNome] = useState('');
+  const [alunos, setAlunos] = useState([]);
+
+  const email = localStorage.getItem('email');
+  const token = localStorage.getItem('token');
+
+  const authorization = {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  }
+
+  useEffect(() => {
+    api.get('api/alunos', authorization)
+      .then(response => {
+        setAlunos(response.data);
+      })
+      .catch(error => {
+        console.error('Erro ao carregar os alunos:', error);
+      });
+  }, []); // Passando um array vazio para executar apenas uma vez
+ 
+  return (
+    <div className='aluno-container'>
+      <header>
+        <img src={logoCadastro} alt='Cadastro'></img>
+        <span>Bem-Vindo, <strong>{email}</strong></span>
+        <Link className='button' to="aluno/novo/0">Novo Aluno</Link>
+        <button type='button'>
+          <FiXCircle size={35} color='#17202a'></FiXCircle>
+        </button>
+      </header>
+      <form>
+        <input type='text' placeholder='Nome'></input>
+        <button type='button' className='button'>
+          Filtrar aluno por nome (parcial)
+        </button>
+      </form>
+      <h1>Relação de Alunos</h1>
+      <ul>
+        {alunos.map(aluno => (
+          <li key={aluno.id}>
+            <b>Nome:</b> {aluno.nome}<br /><br />
+            <b>Email:</b> {aluno.email}<br /><br />
+            <b>Idade:</b> {aluno.idade}<br /><br />
+
+            <button type='button'><FiEdit size={25} color='#17202a' /></button>
+            <button><FiUserX size={25} color='#17202a' /></button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
